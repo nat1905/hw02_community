0a2627379beb5b47aa07e8a90386a02f42b6db47
nat1905 <ershovanatalya@gmail.com>
2022-07-01 17:15:46 +0300

    6 commit

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('posts.urls', namespace='posts')),
     path('group/<slug:slug>/', include('posts.urls', namespace='posts')),
-    path('admin/', admin.site.urls)        
+    path('admin/', admin.site.urls)
 ]
